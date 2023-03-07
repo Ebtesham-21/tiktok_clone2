d8efbcf5781ff47a1e36f912539756d25e72b8a0
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-03-07 16:58:43 +0600

    third day

---
+++ components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import {NextPage} from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import GoogleLogin from 'react-google-login';
+import {AiFillHome, AiOutlineMenu, AiFillCloseCircle} from 'react-icons/ai';
+
+
+const Sidebar = () => {
+
+  const [showSidebar, setShowSidebar] = useState(true);
+
+
+  return (
+    <div>
+      <div
+       className="block xl:hidden m-2 ml-4 mt-3 text-xl"
+       onClick = {() => setShowSidebar((prev => !prev))}
+      
+      >
+        
+        {showSidebar ? <AiFillCloseCircle /> : <AiOutlineMenu/>}
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
