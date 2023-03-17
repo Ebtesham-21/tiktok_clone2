02d8d4ecb62e3dd85a7953a06fe1b0179b0ddac1
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-03-17 16:01:55 +0600

    17/3/23

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,6 +8,8 @@ import {BiSearch} from 'react-icons/bi';
 import {IoMdAdd} from 'react-icons/io';
 import Logo from '../utils/utils/tiktik-logo.png';
 
+
+
 const Navbar = () => {
   return (
     <div className="w-full flex justify-between-items-center border-b-2 border-gray-200 py-2 px-4 ">
